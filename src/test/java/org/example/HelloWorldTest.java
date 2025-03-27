9999a52e8d0bb93d50642f5395f274e4f3e67988
jfrecio0157 <jfrecio@gmail.com>
2025-03-27 15:14:56 +0100

    Subida test con JUnit 4

--- src/test/java/org/example/HelloWorldTest.java
+++ src/test/java/org/example/HelloWorldTest.java
@@ -1,16 +1,22 @@
 package org.example;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 //El test ha sido creado entrando en main->java->org.example-> HelloWorld.
 //Boton derecho sobre el nombre de la clase->Generate-> Test -> Y he elegido Junit4 y marcado el metodo helloWorld
 
-//Para una prueba POJO, la clase debe tener como nombre xxxTest y el metodo testxxx
-//Para ejecutarlo. Pulsar en el menu Maven -> test
+//Para una prueba Junit4, la clase debe tener como nombre xxxTest y delante del metodo hay que poner @Test
+//Incluir en el pom.xml la dependencia de Junit4
 
+//Para ejecutarlo. Pulsar en el menu Maven -> test
 public class HelloWorldTest {
 
-    public void testgethello() {
+    @Test
+    public void gethello() {
         HelloWorld helloWorld = new HelloWorld();
-        assert ("Hello World").equals(helloWorld.gethello());
 
+        assertEquals("Hello World",helloWorld.gethello());
     }
 }
